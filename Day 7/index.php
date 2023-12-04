368c1da2468ac8b8e792c9773546c8ae0ac12f91
Theo Maerten <theo.maerten@sga-industires.com>
2023-12-04 07:44:51 +0100

    day7

--- Day 7/index.php
+++ Day 7/index.php
@@ -6,31 +6,59 @@ debugMode(true);
 $contenu = file_get_contents('data.txt');
 $contenu = explode("\n", $contenu);
 
-
 // Initialisation du tableau résultant
 $tableauxResultants = [];
+// Fonction récursive pour calculer la taille d'un dossier
+function calculerTailleDossier($dossier) {
+    $taille = 0;
+    foreach ($dossier as $element) {
+        // Vérifier si l'élément est un tableau (sous-dossier)
+        if (is_array($element)) {
+            // Si c'est un sous-dossier, ajouter la taille du sous-dossier
+            $taille += calculerTailleDossier($element);
+        } else {
+            // Si l'élément est un fichier, ajouter sa taille
+            $taille += intval($element);
+        }
+    }
+    return $taille;
+}
+
+// Variables pour suivre le dossier précédent
+$dossierPrecedent = "";
 $sousTableauActuel = [];
 
 // Parcours du tableau initial
 foreach ($contenu as $element) {
     // Vérifier si l'élément commence par "$ cd" suivi de caractères autres que ".."
     if (strpos($element, "$ cd") === 0 && strpos($element, "$ cd ..") !== 0) {
-        // Si un sous-tableau actuel existe, l'ajouter au tableau résultant
+        // Si un sous-tableau actuel existe, calculer sa taille et l'ajouter au tableau résultant
         if (!empty($sousTableauActuel)) {
-            $tableauxResultants[] = $sousTableauActuel;
-            $sousTableauActuel = []; // Réinitialiser le sous-tableau actuel
+            $taille = calculerTailleDossier($sousTableauActuel);
+            $tableauxResultants[] = [$dossierPrecedent => $taille];
         }
+        
+
+        // Réinitialiser le sous-tableau actuel et mettre à jour le dossier précédent
+        $sousTableauActuel = [];
+        $dossierPrecedent = substr($element, 5);
+
+
+    } else {
+        // Ajouter l'élément actuel au sous-tableau actuel
+        $sousTableauActuel[] = $element;
     }
 
-    // Ajouter l'élément actuel au sous-tableau actuel
-    $sousTableauActuel[] = $element;
 }
 
-// Ajouter le dernier sous-tableau actuel au tableau résultant
+// Ajouter la taille du dernier sous-tableau actuel au tableau résultant
 if (!empty($sousTableauActuel)) {
-    $tableauxResultants[] = $sousTableauActuel;
+    $taille = calculerTailleDossier($sousTableauActuel);
+    $tableauxResultants[] = [$dossierPrecedent => $taille];
 }
 
 // Afficher les résultats
-    dd($tableauxResultants);
+dd($tableauxResultants);
 
+
+//FAIRE AVEC DES CLASS 
