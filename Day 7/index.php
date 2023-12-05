93384266366b61d7f848bd6126fe5a2d8431ffed
Theo Maerten <theo.maerten@sga-industires.com>
2023-12-05 08:11:58 +0100

    on y est presque cest la bonne methode

--- Day 7/index.php
+++ Day 7/index.php
@@ -6,59 +6,73 @@ debugMode(true);
 $contenu = file_get_contents('data.txt');
 $contenu = explode("\n", $contenu);
 
-// Initialisation du tableau résultant
-$tableauxResultants = [];
-// Fonction récursive pour calculer la taille d'un dossier
-function calculerTailleDossier($dossier) {
-    $taille = 0;
-    foreach ($dossier as $element) {
-        // Vérifier si l'élément est un tableau (sous-dossier)
-        if (is_array($element)) {
-            // Si c'est un sous-dossier, ajouter la taille du sous-dossier
-            $taille += calculerTailleDossier($element);
-        } else {
-            // Si l'élément est un fichier, ajouter sa taille
-            $taille += intval($element);
-        }
+class Dossier
+{
+    public $nom;
+    public $taille;
+    public $sousDossiers = [];
+
+    public function __construct($nom, $taille)
+    {
+        $this->nom = $nom;
+        $this->taille = $taille;
     }
+
+    public function ajouterSousDossier($sousDossier)
+    {
+        $this->sousDossiers[] = $sousDossier;
+    }
+}
+
+class Fichier
+{
+    public $nom;
+    public $taille;
+
+    public function __construct($nom, $taille)
+    {
+        $this->nom = $nom;
+        $this->taille = $taille;
+    }
+}
+
+// Initialisation du dossier racine
+$tableauFin = new Dossier("test", 0);
+
+// Fonction récursive pour calculer la taille d'un dossier
+function calculerTailleDossier($dossier)
+{
+    $taille = $dossier->taille;
+
+    foreach ($dossier->sousDossiers as $sousDossier) {
+        $taille += calculerTailleDossier($sousDossier);
+    }
+
     return $taille;
 }
 
 // Variables pour suivre le dossier précédent
-$dossierPrecedent = "";
-$sousTableauActuel = [];
+$dossierActuel = $tableauFin;
 
 // Parcours du tableau initial
 foreach ($contenu as $element) {
     // Vérifier si l'élément commence par "$ cd" suivi de caractères autres que ".."
     if (strpos($element, "$ cd") === 0 && strpos($element, "$ cd ..") !== 0) {
-        // Si un sous-tableau actuel existe, calculer sa taille et l'ajouter au tableau résultant
-        if (!empty($sousTableauActuel)) {
-            $taille = calculerTailleDossier($sousTableauActuel);
-            $tableauxResultants[] = [$dossierPrecedent => $taille];
-        }
-        
+        // Calculer la taille du dossier actuel
+        $taille = calculerTailleDossier($dossierActuel);
 
-        // Réinitialiser le sous-tableau actuel et mettre à jour le dossier précédent
-        $sousTableauActuel = [];
-        $dossierPrecedent = substr($element, 5);
+        // Ajouter le dossier actuel au dossier précédent
+        $dossierPrecedent = new Dossier(substr($element, 5), $taille);
+        $dossierActuel->ajouterSousDossier($dossierPrecedent);
 
-
+        // Mettre à jour le dossier actuel
+        $dossierActuel = $dossierPrecedent;
     } else {
-        // Ajouter l'élément actuel au sous-tableau actuel
-        $sousTableauActuel[] = $element;
+        // Ajouter un fichier au dossier actuel
+        $fichier = new Fichier("", intval($element));
+        $dossierActuel->ajouterSousDossier($fichier);
     }
-
-}
-
-// Ajouter la taille du dernier sous-tableau actuel au tableau résultant
-if (!empty($sousTableauActuel)) {
-    $taille = calculerTailleDossier($sousTableauActuel);
-    $tableauxResultants[] = [$dossierPrecedent => $taille];
 }
 
 // Afficher les résultats
-dd($tableauxResultants);
-
-
-//FAIRE AVEC DES CLASS 
+dd($tableauFin);
